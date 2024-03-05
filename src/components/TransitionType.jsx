aaca5adc0f33414d37a0ea9a6c8c122e93b67106
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-05 18:52:06 +0600

    add some code

--- src/components/TransitionType.jsx
+++ src/components/TransitionType.jsx
@@ -1,15 +1,52 @@
-import { motion } from 'framer-motion'
-import React from 'react'
+import { motion } from "framer-motion";
+import React from "react";
 
 export default function TransitionType() {
   return (
-    <motion.div
-    initial={{opacity:0, x:"-100vh"}}
-    animate={{opacity:1, x:0}}
-    >
-      <h1>Learning Animation</h1>
-      <p>We love animation. That's why we Learning it. Framer Motion is Great !!</p>
+    // Framer Motion Transition type
+    // 1)default transition type is "tweens";
+    <div>
+      <motion.div
+        className=" m-4"
+        initial={{ opacity: 0, x: "-100vh" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2 }}
+      >
+        <h1>Learning Animation</h1>
+        <p>
+          We love animation. That's why we Learning it. Framer Motion is Great
+          !!
+        </p>
+      </motion.div>
+      {/* Framer Motion Transition type
+        2)Animation type is  "spring with bounce"; */}
+      <motion.div
+        className=" m-4"
+        initial={{ opacity: 0, x: "-100vh" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2, type: "spring", bounce: 0.5 }}
+      >
+        <h1>Learning Animation</h1>
+        <p>
+          We love animation. That's why we Learning it. Framer Motion is Great
+          !!
+        </p>
+      </motion.div>
 
-    </motion.div>
-  )
+      {/* Framer Motion Transition type
+        2)Animation type is  "spring without bounce"; */}
+      <motion.div
+        className=" m-4"
+        initial={{ opacity: 0, x: "-100vh" }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 2, type: "spring" }}
+      >
+        <h1>Learning Animation</h1>
+        <p>
+          We love animation. That's why we Learning it. Framer Motion is Great
+          !!
+        </p>
+      </motion.div>
+    </div>
+  );
 }
