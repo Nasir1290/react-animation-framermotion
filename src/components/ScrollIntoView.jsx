aa3722a73afb46c33689a79b7a7c4c9dd42ec72a
Nasir1290 <nasirmia12901290@gmail.com>
2024-03-06 07:15:16 +0600

    add some code

--- src/components/ScrollIntoView.jsx
+++ src/components/ScrollIntoView.jsx
@@ -1,21 +1,40 @@
-import { motion } from 'framer-motion'
-import React from 'react'
- 
+import { motion } from "framer-motion";
+import React from "react";
+
 export default function ScrollIntoView() {
-    const greetings = ['Hello', 'Hi', 'Hey', 'Hola','salam', 'morning', 'evening'];
+  const greetings = [
+    "Hello",
+    "Hi",
+    "Hey",
+    "Hola",
+    "salam",
+    "morning",
+    "evening",
+  ];
   return (
-    <div>
-      {greetings.map((greeting,index) => (
-        <ScrollItem key={index} text={greeting}/>
+    <div className=" ">
+      {greetings.map((greeting, index) => (
+        <ScrollItem key={index} text={greeting} />
       ))}
     </div>
-  )
+  );
 }
 
-const ScrollItem = ({text}) => {
-    return (
-        <div>
-            {text}
-        </div>
-    )
-}
+const ScrollItem = ({ text }) => {
+  return (
+    <motion.div
+      className=" text-lg bg-sky-500 w-36 h-36 rounded-md mb-4 text-center"
+      initial={{ opacity: 0, x: "-50vh" }}
+      whileInView={{
+        opacity: 1,
+        x: 0,
+        transition: {
+          duration: 1.5,
+        },
+      }}
+      viewport={{ once: false }}
+    >
+      {text}
+    </motion.div>
+  );
+};
